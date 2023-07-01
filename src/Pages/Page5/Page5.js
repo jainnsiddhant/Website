79c50e51f3039937141047ea849bd58d8887ab00
Kartikey Bhardwaj <shyamsharmamohan444@gmail.com>
2023-07-01 18:57:30 +0530

    Update Page5.js

--- src/Pages/Page5/Page5.js
+++ src/Pages/Page5/Page5.js
@@ -1,12 +1,15 @@
 import React from 'react'
 import Navbar from '../../Components/Navbar/Navbar'
 import { Footer } from '../../Components/Footer/Footer'
+import Page5templates from '../../Components/Page5/Page5templates'
+import Page5Table from '../../Components/Page5/Page5Table'
 
 const Page5 = () => {
   return (
     <>
         <Navbar />
-        <div>Page5</div>
+        <Page5templates />
+        <Page5Table />
         <Footer />
     </>
   )
