0f876272d11e14bc9f248d60b804f7e9b91a3fcd
PrishaThapar06 <prisha6thapar@gmail.com>
2024-02-13 18:11:06 +0530

    scholar

---
+++ src/Pages/NewScholar/NewScholarPg.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import Navbar from '../../Components/Navbar/Navbar'
+import NewFooter from '../../Components/NewFooter/NewFooter'
+import NewScholars from '../../Components/NewScholarship/NewScholars'
+
+const Scholarship = () => {
+  return (
+    <>
+        <Navbar />
+        <NewScholars />
+        <NewFooter />
+    </>
+  )
+}
+
+export default Scholarship
