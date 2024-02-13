5f02d450c355d1d80fc6eb5d4ce84dce5cf7f52f
PrishaThapar06 <prisha6thapar@gmail.com>
2024-02-13 18:39:41 +0530

    updates

--- src/Components/NewScholarship/NewScholars.jsx
+++ src/Components/NewScholarship/NewScholars.jsx
@@ -7,28 +7,51 @@ const NewScholars = () => {
 <div className='flex'>
 <aside id="logo-sidebar" class="fixed top-0 left-0 z-40 w-64 h-screen transition-transform -translate-x-full sm:translate-x-0" aria-label="Sidebar">
    <div class="h-full px-3 py-4 overflow-y-auto bg-gray-50">
-   <button type="submit" class="inline-flex items-center py-2.5 px-3 ms-2 text-sm font-medium text-white bg-blue-700 rounded-lg border border-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300">
-        <svg class="w-4 h-4 me-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 20">
-            <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z"/>
-        </svg>Search
-    </button>
+   {/* Search Button */}
+   <div>
+         <form>   
+           <label for="default-search" class="mb-2 text-sm font-medium text-gray-900 sr-only w-full">Search</label>
+             <div class="relative">
+               {/* <div class="absolute inset-y-0 start-0 flex items-center ps-3 pointer-events-none">
+                 <svg class="w-4 h-4 text-gray-500" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 20">
+                 <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z"/>
+                 </svg>
+               </div> */}
+               <input type="search" id="default-search" class="block w-full p-4 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-400 focus:border-blue-400" placeholder="Search" required />
+               <button type="submit" class="text-white absolute end-2.5 bottom-2.5 bg-blue-500 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2">
+               <svg class="w-4 h-4 text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 20">
+                 <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z"/>
+                 </svg>
+               </button>
+            </div>
+         </form>
+     </div>
+    
       <div class="space-y-2 font-medium mt-2">
          
-        <div class="flex items-center mb-4">
-            <input id="default-checkbox" type="checkbox" value="" class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500" />
-            <label for="default-checkbox" class="ms-2 text-sm font-medium text-gray-900">Default checkbox</label>
-        </div>
         <div class="flex items-center">
-        <input checked id="checked-checkbox" type="checkbox" value="" class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500" />
-        <label for="checked-checkbox" class="ms-2 text-sm font-medium text-gray-900">Checked state</label>
-      </div>
-
+            <input type="checkbox" value="" class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500" />
+            <label class="ms-2 text-sm font-medium text-gray-900">oxford</label>
+        </div>
+        <div class="flex items-center mb-4">
+            <input type="checkbox" value="" class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500" />
+            <label class="ms-2 text-sm font-medium text-gray-900">Harvard</label>
+        </div>
+        <div class="flex items-center mb-4">
+            <input type="checkbox" value="" class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500" />
+            <label class="ms-2 text-sm font-medium text-gray-900">princeton</label>
+        </div>
+        <div class="flex items-center mb-4">
+            <input type="checkbox" value="" class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500" />
+            <label class="ms-2 text-sm font-medium text-gray-900">goldmansachs</label>
+        </div>
+        
       </div>
    </div>
 </aside>
 
 <div className='flex flex-col w-[80%] mx-auto'>
-<div className="text-xl mt-2 mb-4">Student Ambassadors</div>
+<div className="text-xl mt-2 mb-4">Scholarships</div>
 
          {/* Search Button */}
          <div className="mb-10">
