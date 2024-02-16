abaf52983ce245e3ab2953c60dd97f6880456378
PrishaThapar06 <prisha6thapar@gmail.com>
2024-02-16 18:58:31 +0530

    course

--- src/Components/NewScholarship/NewScholars.jsx
+++ src/Components/NewScholarship/NewScholars.jsx
@@ -1,205 +1,160 @@
-import React from "react";
-import data from "./data";
+import React , {useMemo} from 'react'
+import { useReactTable , getCoreRowModel , flexRender } from '@tanstack/react-table'
+import mData from "./MockData.json"
+import './NewScholars.css';
+import { motion } from 'framer-motion';
+
 const NewScholars = () => {
+    const data = useMemo(() => mData, [])
+
+    const columns = [
+        {
+            header: "ID",
+            accessorKey: "id",
+            footer: "ID",
+        },
+        {
+            header: "Name",
+            accessorKey: "name",
+            footer: "Name",
+        },
+        {
+            header: "Start Time",
+            accessorKey: "startTime",
+            footer: "Start Time",
+        },
+        {
+            header: "End Time",
+            accessorKey: "endTime",
+            footer: "End Time",
+        },
+        {
+            header: "Price",
+            accessorKey: "price",
+            footer: "Price",
+        },
+        {
+            header: "Link",
+            accessorKey: "action",
+            footer: "Link",
+        },
+    ]
+
+    const table = useReactTable({data, columns, getCoreRowModel: getCoreRowModel()})
   return (
-    <>
-      <div className="flex mb-8">
-        <aside
-          id="logo-sidebar"
-          class="fixed top-0 left-0 z-40 w-64 h-screen transition-transform -translate-x-full sm:translate-x-0"
-          aria-label="Sidebar"
-        >
-          <div class="h-full px-3 py-4 overflow-y-auto bg-gray-50">
-            {/* Search Button */}
-            <div>
-              <form>
-                <label
-                  for="default-search"
-                  class="mb-2 text-sm font-medium text-gray-900 sr-only w-full"
-                >
-                  Search
-                </label>
-                <div class="relative">
-                  {/* <div class="absolute inset-y-0 start-0 flex items-center ps-3 pointer-events-none">
-                 <svg class="w-4 h-4 text-gray-500" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 20">
-                 <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z"/>
-                 </svg>
-               </div> */}
-                  <input
-                    type="search"
-                    id="default-search"
-                    class="block w-full p-4 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-400 focus:border-blue-400"
-                    placeholder="Search"
-                    required
-                  />
-                  <button
-                    type="submit"
-                    class="text-white absolute end-2.5 bottom-2.5 bg-blue-500 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2"
-                  >
-                    <svg
-                      class="w-4 h-4 text-white"
-                      aria-hidden="true"
-                      xmlns="http://www.w3.org/2000/svg"
-                      fill="none"
-                      viewBox="0 0 20 20"
-                    >
-                      <path
-                        stroke="currentColor"
-                        stroke-linecap="round"
-                        stroke-linejoin="round"
-                        stroke-width="2"
-                        d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z"
-                      />
-                    </svg>
-                  </button>
-                </div>
-              </form>
-            </div>
-
-            <div class="space-y-2 font-medium mt-2">
-              <div class="flex items-center">
-                <input
-                  type="checkbox"
-                  value=""
-                  class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500"
-                />
-                <label class="ms-2 text-sm font-medium text-gray-900">
-                  oxford
-                </label>
-              </div>
-              <div class="flex items-center mb-4">
-                <input
-                  type="checkbox"
-                  value=""
-                  class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500"
-                />
-                <label class="ms-2 text-sm font-medium text-gray-900">
-                  Harvard
-                </label>
-              </div>
-              <div class="flex items-center mb-4">
-                <input
-                  type="checkbox"
-                  value=""
-                  class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500"
-                />
-                <label class="ms-2 text-sm font-medium text-gray-900">
-                  princeton
-                </label>
-              </div>
-              <div class="flex items-center mb-4">
-                <input
-                  type="checkbox"
-                  value=""
-                  class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500"
-                />
-                <label class="ms-2 text-sm font-medium text-gray-900">
-                  goldmansachs
-                </label>
-              </div>
-            </div>
-          </div>
-        </aside>
-
-        <div className="flex flex-col w-[80%] mx-auto">
-          <div className="text-xl mt-2 mb-4">Scholarships</div>
-
-          {/* Search Button */}
-          <div className="mb-10">
-            <form>
-              <label
-                for="default-search"
-                class="mb-2 text-sm font-medium text-gray-900 sr-only w-full"
-              >
-                Search
-              </label>
-              <div class="relative">
-                <div class="absolute inset-y-0 start-0 flex items-center ps-3 pointer-events-none">
-                  <svg
-                    class="w-4 h-4 text-gray-500"
-                    aria-hidden="true"
-                    xmlns="http://www.w3.org/2000/svg"
-                    fill="none"
-                    viewBox="0 0 20 20"
-                  >
-                    <path
-                      stroke="currentColor"
-                      stroke-linecap="round"
-                      stroke-linejoin="round"
-                      stroke-width="2"
-                      d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z"
-                    />
-                  </svg>
-                </div>
-                <input
-                  type="search"
-                  id="default-search"
-                  class="block w-full p-4 ps-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-400 focus:border-blue-400"
-                  placeholder="Search University"
-                  required
-                />
-                <button
-                  type="submit"
-                  class="text-white absolute end-2.5 bottom-2.5 bg-blue-500 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2"
-                >
-                  Search
-                </button>
-              </div>
-            </form>
-          </div>
-
-          <div class="relative overflow-x-auto shadow-md sm:rounded-lg">
-            <table class="w-full text-sm text-left rtl:text-right text-gray-500">
-              <thead class="text-xs text-gray-700 uppercase bg-gray-50">
-                <tr>
-                  <th scope="col" class="px-6 py-3">
-                    Name
-                  </th>
-                  <th scope="col" class="px-6 py-3">
-                    Start date
-                  </th>
-                  <th scope="col" class="px-6 py-3">
-                    End date
-                  </th>
-                  <th scope="col" class="px-6 py-3">
-                    Price
-                  </th>
-                  <th scope="col" class="px-6 py-3">
-                    Action
-                  </th>
-                </tr>
-              </thead>
-              <tbody>
-                {data.map((item, index) => {
-                  return (
-                    <tr class="odd:bg-white odd:dark:bg-gray-900 even:bg-gray-50">
-                      <th
-                        scope="row"
-                        class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap"
-                      >
-                        {item.name}
-                      </th>
-                      <td class="px-6 py-4">{item.startTime}</td>
-                      <td class="px-6 py-4">{item.endTime}</td>
-                      <td class="px-6 py-4">$2999</td>
-                      <td class="px-6 py-4">
-                        <a
-                          href="#"
-                          class="font-medium text-blue-600 hover:underline"
-                        >
-                          {item.action}
-                        </a>
-                      </td>
-                    </tr>
-                  );
-                })}
-              </tbody>
-            </table>
-          </div>
+    <> 
+       <div className='w-[80%] mx-auto'>
+       <div className="py-4 px-4">
+                    <h2 data-aos='fade-right' className='mb-3 text-4xl font-extrabold tracking-tight leading-none text-gray-900 md:text-5xl lg:text-6xl'>
+                        All Scholarships
+                    </h2>
+                    <p className="mb-3 text-lg font-normal text-gray-500 lg:text-xl sm:px-16 lg:px-48">
+                        <div className='space-x-3'>
+                        <button 
+                                    type="button" 
+                                    style={{
+                                            color: 'white',
+                                            backgroundImage: 'linear-gradient(to right, #3b82f6, #2563eb)',
+                                            boxShadow: '0 0 0 1px rgba(66, 153, 225, 0.5), 0 2px 4px 0 rgba(0, 0, 0, 0.05)',
+                                            borderRadius: '0.375rem',
+                                            fontSize: '0.875rem',
+                                            padding: '0.625rem 1.25rem',
+                                            textAlign: 'center',
+                                            marginRight: '0.5rem',
+                                            marginBottom: '0.5rem',
+                                            outline: 'none',
+                                            transition: 'background-color 0.2s ease-in-out, box-shadow 0.2s ease-in-out',
+                                          }} 
+                                          className='text-lg'
+                                    onClick={() => window.open('www.google.com', '_blank')}
+                                    >
+                                    All
+                                </button>
+                                 <button 
+                                    type="button" 
+                                    style={{
+                                            color: 'white',
+                                            backgroundImage: 'linear-gradient(to right, #3b82f6, #2563eb)',
+                                            boxShadow: '0 0 0 1px rgba(66, 153, 225, 0.5), 0 2px 4px 0 rgba(0, 0, 0, 0.05)',
+                                            borderRadius: '0.375rem',
+                                            fontSize: '0.875rem',
+                                            padding: '0.625rem 1.25rem',
+                                            textAlign: 'center',
+                                            marginRight: '0.5rem',
+                                            marginBottom: '0.5rem',
+                                            outline: 'none',
+                                            transition: 'background-color 0.2s ease-in-out, box-shadow 0.2s ease-in-out',
+                                          }} 
+                                          className='text-lg'
+                                    onClick={() => window.open('www.google.com', '_blank')}
+                                    >
+                                    Active
+                                </button>
+                                 <button 
+                                    type="button" 
+                                    style={{
+                                            color: 'white',
+                                            backgroundImage: 'linear-gradient(to right, #3b82f6, #2563eb)',
+                                            boxShadow: '0 0 0 1px rgba(66, 153, 225, 0.5), 0 2px 4px 0 rgba(0, 0, 0, 0.05)',
+                                            borderRadius: '0.375rem',
+                                            fontSize: '0.875rem',
+                                            padding: '0.625rem 1.25rem',
+                                            textAlign: 'center',
+                                            marginRight: '0.5rem',
+                                            marginBottom: '0.5rem',
+                                            outline: 'none',
+                                            transition: 'background-color 0.2s ease-in-out, box-shadow 0.2s ease-in-out',
+                                          }} 
+                                          className='text-lg'
+                                    onClick={() => window.open('www.google.com', '_blank')}
+                                    >
+                                    Archived
+                                </button>
+                        </div>
+                    </p>
         </div>
-      </div>
+        <table>
+            <thead>
+               {table.getHeaderGroups().map(headerGroup => (
+                   <tr key={headerGroup.id}>
+                       {headerGroup.headers.map(header =>(
+                           <th key={header.id}>
+                              {flexRender(
+                                   header.column.columnDef.header , header.getContext()
+                               )}
+                           </th>
+                       ))}
+                   </tr>
+               ))}
+            </thead>
+            <tbody>
+              {table.getRowModel().rows.map(row => (
+                <tr key={row.id}>
+                    {row.getVisibleCells().map(cell => (
+                        <td key={cell.id}>
+                            {flexRender(cell.column.columnDef.cell , cell.getContext())}
+                        </td>
+                    ))}
+                </tr>
+              ))}
+            </tbody>
+            <tfoot>
+               {table.getFooterGroups().map(footerGroup => (
+                   <tr key={footerGroup.id}>
+                       {footerGroup.headers.map(header =>(
+                           <th key={header.id}>
+                              {flexRender(
+                                   header.column.columnDef.header , header.getContext()
+                               )}
+                           </th>
+                       ))}
+                   </tr>
+               ))}
+            </tfoot>
+        </table>
+       </div>
     </>
-  );
-};
+  )
+}
 
-export default NewScholars;
+export default NewScholars
