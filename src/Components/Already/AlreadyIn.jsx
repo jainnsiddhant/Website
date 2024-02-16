375548b988c92a2c91eaa7e60cf9485c342f0921
PrishaThapar06 <prisha6thapar@gmail.com>
2024-02-16 21:30:02 +0530

    change

--- src/Components/Already/AlreadyIn.jsx
+++ src/Components/Already/AlreadyIn.jsx
@@ -21,10 +21,10 @@ const AlreadyIn = () => {
             <section>
                 <div className="py-4 px-4 mx-auto max-w-screen-xl text-center lg:py-16">
                     <h2 data-aos='fade-right' className='mb-3 text-4xl font-extrabold tracking-tight leading-none text-gray-900 md:text-5xl lg:text-6xl'>
-                        Already In UK Guidelines
+                        Already In UK
                     </h2>
                     <p className="mb-3 text-lg font-normal text-gray-500 lg:text-xl sm:px-16 lg:px-48">
-                        Welcome to our comprehensive guide.
+                    Unlock Your Path: Essential Guidance for Navigating Life in the UK.
                     </p>
                 </div>
 
