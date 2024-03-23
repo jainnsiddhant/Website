e0f1375e4898e678871f75fec8b16d649dcaac5b
Kartikey353 <shyamsharmamohan444@gmail.com>
2024-03-23 17:19:45 +0530

    adding new chnages

--- src/Components/NewConnect/Template.js
+++ src/Components/NewConnect/Template.js
@@ -1,20 +1,23 @@
 import React from "react";
-import resumebg from "../../images/resumebg.jpg";
+import template from "../../images/template.jpg";
+import DownloadPDF4 from "../Download/DownloadPDF4.js";
 
-export default function Banner() {
+export default function Template() {
     return (
-        <div id="banner" className='h-full w-[80%] mx-auto bg-cover bg-center p-20' style={{backgroundImage: `linear-gradient(rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.5)), url(${resumebg})` , backgroundRepeat: 'no-repeat' , backgroundSize: 'cover', backdropFilter: 'blur(10px)'}}>
+        <div id="temp" className='h-full w-[80%] mx-auto bg-cover bg-center p-20 rounded-md' style={{ backgroundImage: `linear-gradient(rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.5)), url(${template})` , backgroundRepeat: 'no-repeat' , backgroundSize: 'cover', backdropFilter: 'blur(10px)'}}>
             <div className="flex flex-col items-center justify-center">
                 <h1 className="mt-10 text-4xl font-bold text-center text-white">
-                    Resume Builder
+                    Template to send connect request
                 </h1>
-                <p className="text-lg text-center text-white mt-5 w-[90%]">
-                Unlock your career potential effortlessly with our Resume Builder. Craft professional resumes that stand out and make a lasting impression on potential employers.
+                <p className="text-lg text-center mt-5 w-[90%] text-white">
+                    Discover vibrant connections with student ambassadors and delve into captivating featured blogs on our platform.
+                    Discover vibrant connections with student ambassadors and delve into captivating featured blogs on our platform.
                 </p>
-                <div className="mt-8 mb-10">
-                    <button className="px-6 py-2 text-center text-white rounded-md shadow-md" style={{backgroundColor: '#2563EB'}}>
-                        <a href="#resumebuild">Get Started</a>
-                    </button>
+                <div className="mt-8 mb-10 space-x-2">
+                    {/* <button className="px-6 py-2 text-center text-white rounded-md shadow-md" style={{backgroundColor: '#2563EB'}}> */}
+                        {/* <a href="#connectambassador">Download File</a> */}
+                        <DownloadPDF4 />
+                    {/* </button> */}
                 </div>
             </div>
         </div>
